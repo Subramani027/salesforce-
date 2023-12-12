4a3081bdfe1b7e756b369dccfd27e73e5994f3e5
Subramani M <150350346+Subramani027@users.noreply.github.com>
2023-12-12 19:24:28 +0530

    Update exercise-08Dec2023.js

--- exercise-08Dec2023.js
+++ exercise-08Dec2023.js
@@ -1,5 +1,3 @@
-let data
-
 import('./moviesPlay.js')
 	.then(res => {
 		console.log('data imported into data constant');
@@ -10,7 +8,24 @@ import('./moviesPlay.js')
 function run() {
 	const movies = data.movies;
 	console.log('Number of movies: ' + movies.length);
-	for (let i = 0; i < movies.length; i++) {
-		console.log(movies[i].originalLanguage);
-	}
-}
+	// for (let i = 0; i < movies.length; i++) {
+	// 	// console.log(movies[i].originalLanguage);
+	// 	let tableHTML = "<table class='ui celled table'>";
+	// 	tableHTML += "<thead><tr><th>Language</th><th>Count</th></tr></thead>";
+	// 	tableHTML += "<tbody>";
+	// 	tableHTML += `<tr><td>${movies[i].originalLanguage} (${movies[i].originalLanguage})</td></tr>`;	
+	// 	tableHTML += "</tbody></table>";
+	// 	document.body.innerHTML = tableHTML;
+
+		
+	// }
+	const movieInfoDiv = document.getElementById('table');
+	let tableHTML = '';
+	const mpp = movies.map((object) =>{
+		console.log(object)
+		tableHTML += "<thead><tr><th>Language</th><th>Count</th></tr></thead>";
+		tableHTML += "<tbody>";
+		tableHTML += `<tr><td>${object.originalLanguage} (${object.spokenLanguages})</td><td>here i have to display count</td></tr>`;	
+		tableHTML += "</tbody>"
+	})
+	movieInfoDiv.innerHTML = "<table class='ui celled table'>" + tableHTML +"</table>";
